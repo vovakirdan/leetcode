45ee31d4c59cf17fdd7678c63d86f5953c012ac1
vovakirdan <vova.kirdan@mail.ru>
2025-04-10 13:43:37 +0300

    Add solution for LeetCode problem 199: Binary Tree Right Side View
    - Implemented the algorithm in Go to return the values of nodes visible from the right side of a binary tree.
    - Added a detailed README with problem description, algorithm steps, complexity analysis, and example.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Решение задач на leetcode
 
 * [199. Binary Tree Right Side View](solutions/199. Binary Tree Right Side View/readme)
-# leetcode
---
+++ solutions/199. Binary Tree Right Side View/main.go
@@ -0,0 +1,47 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	
+	queue := []*TreeNode{root}
+	result := []int{}
+
+	for len(queue) > 0 {
+        level_size := len(queue)
+
+        for i := 0; i < level_size; i++ {
+            node := queue[0]
+            queue = queue[1:]
+
+            if i == level_size - 1 {
+                result = append(result, node.Val)
+            }
+
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+            }
+
+            if node.Right != nil {
+                queue = append(queue, node.Right)
+            }
+        }
+    }
+
+    return result
+}
